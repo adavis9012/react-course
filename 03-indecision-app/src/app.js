f4cffa8db5452b6f17a13d4586cb97a99571a8dc
Andres David Velandia Suarez <andres.velandia@globant.com>
2018-10-22 17:03:59 -0500

    Sec-5,40: The Staetless Functional Component: Some Class components was changed to Stateless

---
+++ 03-indecision-app/src/app.js
@@ -0,0 +1,137 @@
+class IndecisionApp extends React.Component {
+    constructor({props}){
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            options: []
+        }
+    }
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            };
+        });
+    }
+    handlePick(){
+        const choice = Math.floor(Math.random()*this.state.options.length);
+        alert(this.state.options[choice]);
+    }
+    handleAddOption(option){
+        if(option.length < 1){
+            return 'Enter a valid option'
+        } else if(this.state.options.indexOf(option) > -1){
+            return 'Option is already defined'
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            };
+        });
+    }
+    render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        return (
+            <section>
+                <Header title={title} subtitle={subtitle}></Header>
+                <Action handlePick={this.handlePick} hasOptions={this.state.options.length > 0}></Action>
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions = {this.handleDeleteOptions}
+                ></Options>
+                <AddOption 
+                    handleAddOption={this.handleAddOption}
+                ></AddOption>
+            </section>
+        )
+    }
+}
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    );
+}
+
+const Action = (props) => {
+    return (
+        <div>
+            <button 
+                onClick={props.handlePick}
+                disabled={!props.hasOptions}
+            >
+                What Should I do?
+            </button>
+        </div>
+    );
+}
+
+const Options = (props) => {
+    return (
+        <div>
+            <button
+                onClick={props.handleDeleteOptions}
+            >
+                Remove All
+            </button>
+
+            {props.options.map(option => {
+                return <Option key={option} optionText={option}/>;
+            })}
+        </div>
+    );
+}
+
+const Option = (props) => {
+    return (
+        <p>
+            {props.optionText}
+        </p>
+    );
+}
+
+class AddOption extends React.Component {
+    constructor({props}){
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        }
+    }
+    handleAddOption(e){
+        e.preventDefault(); //Prevent Refresh
+        const option = e.target.elements.option.value.trim();
+        const error =  this.props.handleAddOption(option);
+        this.setState(() => {
+            return {error}
+        });
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option"></input>
+                    <input type="submit" value="Add Option"></input>
+                </form>
+                {this.state.error && <p>{this.state.error}</p>}
+            </div>
+        );
+    }
+}
+
+// const User = (props) => {
+//     return (
+//         <div>
+//             <p>Name: {props.name}</p>
+//             <p>Age: {props.age}</p>
+//         </div>
+//     )
+// };
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- README.md
+++ README.md
@@ -5,13 +5,38 @@ Table of contents
 
 * [ 01-Indecision App](#h1-01-indecision-app)
     * [Steps](#h2-steps)
+* [ 02-Indecision App](#h1-02-indecision-app)
+    * [Steps](#h2-steps)
+* [ 03-Indecision App](#h1-03-indecision-app)
+    * [Steps](#h2-steps)
 
 
-# 01-Indecision App
+# 01-Indecision App (Templates)
 ## Steps
 ```
 cd 01-indecision-app
 npm i -g live-server
+npm i -g babel-cli
+npm i -s babel-preset-react@6.24.1 babel-preset-env@1.5.2
+live-server public
+babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+```
+# 02-Indecision App (Components - Class)
+## Steps
+```
+cd 02-indecision-app
+npm i -g live-server
+npm i -g babel-cli
+npm i -s babel-preset-react@6.24.1 babel-preset-env@1.5.2
+live-server public
+babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+```
+# 03-Indecision App (Stateless Functional Components)
+## Steps
+```
+cd 03-indecision-app
+npm i -g live-server
+npm i -g babel-cli
 npm i -s babel-preset-react@6.24.1 babel-preset-env@1.5.2
 live-server public
 babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
