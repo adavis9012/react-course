7a1e4f230877b73934191953127c8e9eefb70286
David Velandia <adavis9012@gmail.com>
2018-10-21 21:54:01 -0500

    Now you can add parametes using attributes

--- 02-indecision-app/src/app.js
+++ 02-indecision-app/src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options =['Thing one', 'Thing two', 'Thing three'];
         return (
             <section>
-                <Header></Header>
+                <Header title={title} subtitle={subtitle}></Header>
                 <Action></Action>
-                <Options></Options>
+                <Options options={options}></Options>
                 <AddOption></AddOption>
             </section>
         )
@@ -15,8 +18,8 @@ class Header extends React.Component {
     render(){
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -37,7 +40,9 @@ class Options extends React.Component {
         return (
             <div>
                 Options component here!
-                <Option></Option>
+                {this.props.options.map(option => {
+                    return <Option key={option} optionText={option}/>;
+                })}
             </div>
         );
     }
@@ -46,9 +51,9 @@ class Options extends React.Component {
 class Option extends React.Component {
     render() {
         return (
-            <div>
-                Option component here
-            </div>
+            <p>
+                {this.props.optionText}
+            </p>
         );
     }
 }
