cffbe6328c7bfb2b00c4a18d4e7727da095acb23
David Velandia <adavis9012@gmail.com>
2018-11-05 14:17:34 -0500

    Sec-5-55: One Component per File, webpack packages was changed to previous version due compressing errors

---
+++ 04-indecision-app/src/components/IndesitionApp.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import AddOption from './AddOptions';
+import Options from './Options';
+import Header from './Header';
+import Action from './Action';
+
+export default class IndecisionApp extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
+        this.state = {
+            options: []
+        }
+    }
+
+    componentDidMount() {
+       try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            if(options) {
+                this.setState(() => ({options}));
+            }
+       } catch (error) {
+           //Do nothing
+       }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount!');
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => ({
+            options: []
+        }));
+    }
+
+    handleDeleteOption(optionToRemove) {
+        this.setState((prevState) => ({
+                options: prevState.options.filter((option) => (
+                    optionToRemove !== option
+                ))
+            })
+        );
+    }
+
+    handlePick(){
+        const choice = Math.floor(Math.random()*this.state.options.length);
+        alert(this.state.options[choice]);
+    }
+    handleAddOption(option){
+        if(option.length < 1){
+            return 'Enter a valid option'
+        } else if(this.state.options.indexOf(option) > -1){
+            return 'Option is already defined'
+        }
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    }
+    render() {
+        return (
+            <section>
+                <Header></Header>
+                <Action handlePick={this.handlePick} hasOptions={this.state.options.length > 0}></Action>
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions = {this.handleDeleteOptions}
+                    handleDeleteOption = {this.handleDeleteOption}
+                ></Options>
+                <AddOption 
+                    handleAddOption={this.handleAddOption}
+                ></AddOption>
+            </section>
+        )
+    }
+}
