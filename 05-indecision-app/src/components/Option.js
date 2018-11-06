e9249935b69bd40669927936ecb55095894dcfbc
David Velandia <adavis9012@gmail.com>
2018-11-06 01:56:19 -0500

    Sec-7-62: Refactoring Other Stateless Functional Components (return removed to use pharentesis)

--- 05-indecision-app/src/components/Option.js
+++ 05-indecision-app/src/components/Option.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <p>
-            {props.optionText}
-            < button 
-                onClick = {(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            > 
-                remove 
-            </button>
-        </p>
-    );
-}
+const Option = (props) => (
+    <p>
+        {props.optionText}
+        < button 
+            onClick = {(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        > 
+            remove 
+        </button>
+    </p>
+);
+
 export default Option;
