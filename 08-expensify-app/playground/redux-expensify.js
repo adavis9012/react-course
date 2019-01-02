5342d92cbe6a644e7fa4d944a3cafcba3aa55c8a
Andres David Velandia <adavis9012@gmail.com>
2019-01-01 22:38:43 -0500

    93. ES6 Spread Operator in Reducers

--- 08-expensify-app/playground/redux-expensify.js
+++ 08-expensify-app/playground/redux-expensify.js
@@ -1,12 +1,35 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({id}) => ({
+  type: 'REMOVE_EXPENSE',
+  expense: {
+    id
+  }
+});
 
 const defaultExpensesReducer = [];
 
 const expensesReducer = (state = defaultExpensesReducer, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+        return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+        return state.filter(({id}) => id !== action.expense.id);
     default:
       return state;
-  };
+  }
 };
 
 const defaultFilterReducer = {
@@ -14,19 +37,28 @@ const defaultFilterReducer = {
   sortBy: 'amount',
   startDate: undefined,
   endDate: undefined
-}
+};
 
 const filterReducer = (state = defaultFilterReducer, action) => {
   switch (action.type) {
     default:
       return state;
-  };
-} 
+  }
+};
 
 const store = createStore(combineReducers({
   expenses: expensesReducer,
   filter: filterReducer
 }));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({description: 'Cofee', amount: 300}));
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 const demoState = {
   expenses: [
@@ -35,7 +67,7 @@ const demoState = {
       description: 'January Rent',
       note: 'This was the final payment for that address',
       amount: 54500,
-      createdAt: 0 
+      createdAt: 0
     }
   ],
   filters : {
@@ -45,5 +77,3 @@ const demoState = {
     endDate: undefined
   }
 };
-
-console.log(store.getState());
